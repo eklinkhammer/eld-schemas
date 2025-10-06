b80cc676c42e9edbc141a9222d41444136858c62
ERIC KLINKHAMMER <eklinkhammer@gmail.com>
2025-10-06 14:05:31 -0700

    Add NewLoginResponse

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ```bash
 npm i git+ssh://git@github.com/eklinkhammer/eld-schemas.git
 ```
+
+# Changes
+Don't forget to change the version.
--- src/schemas/new-login.ts
+++ src/schemas/new-login.ts
@@ -1,9 +1,48 @@
 import { z } from 'zod';
 
+const UrlSchema = z.string().refine(
+  (val) => {
+    try {
+      new URL(val);
+      return true;
+    } catch {
+      return false;
+    }
+  },
+  { message: "Invalid URL" }
+);
 export const NewLoginRequest = z.object({
     dispatcher_id: z.string().min(1),
-    providerUrl: z.string().url(),
+    providerUrl: UrlSchema,
     username: z.string().min(1),
     password: z.string().min(1),
     integrationId: z.string().min(1),
 });
+
+const PublicUser = z.object({
+    id: z.string(),
+    dispatcher_id: z.string(),
+    username: z.string(),
+    created_at: z.coerce.date(),
+    updated_at: z.coerce.date(),
+    integrationId: z.string().min(1),
+});
+type PublicUser = z.infer<typeof PublicUser>;
+
+const PublicProvider = z.object({
+    name: z.string(),
+    url: UrlSchema,
+});
+type PublicProvider = z.infer<typeof PublicProvider>;
+
+export const NewLoginResponseSuccess = z.object({
+    success: z.literal(true),
+    message: z.string(),
+    user: PublicUser,
+    provider: PublicProvider,
+});
+export const NewLoginResponseFailure = z.object({
+    success: z.literal(false),
+    error: z.string(),
+});
+export const NewLoginResponse = z.union([NewLoginResponseSuccess, NewLoginResponseFailure]);
